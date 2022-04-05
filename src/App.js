cc62b75da2e0dd9c963db040d33b3ed74a9e037b
hazrat-abdurahmanov <hezret.abdurehmanov@gmail.com>
2022-04-05 14:56:13 +0400

    Services yaradildi

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar'
 import Header from './components/Header' 
 import AboutMe from './components/AboutMe'
+import Services from './components/Services'
 import ContactUs from './components/ContactUs';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Navbar />
       <Header />
       <AboutMe />
+      <Services />
       <ContactUs />
 
     </div>
---
+++ src/components/Services.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGoogle, faFacebookF } from '@fortawesome/free-brands-svg-icons'
+import { faDesktop, faFileCode } from '@fortawesome/free-solid-svg-icons'
+
+const Services = () => {
+  return (
+    <div className='services'>
+      <h1 className='py-5'>Our services</h1>
+      <div className='container'>
+        <div className="row">
+
+          {/* Google seo xidmetleri */}
+          <div className="col-lg-3 col-md-6 col-sm-6">
+            <div className="box">
+              <div className="circle">
+                <FontAwesomeIcon icon={faGoogle} className='icon' />
+              </div>
+              <h2>SEO</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, possimus.</p>
+            </div>
+          </div>
+
+          {/* Web development ile elaqeli sahe */}
+          <div className="col-lg-3 col-md-6 col-sm-6">
+            <div className="box">
+              <div className="circle">
+                <FontAwesomeIcon icon={faFileCode} className='icon' />
+              </div>
+              <h2>Web development</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, possimus.</p>
+            </div>
+          </div>
+
+          {/* Facebook Marketing SMM */}
+          <div className="col-lg-3 col-md-6 col-sm-6">
+            <div className="box">
+              <div className="circle">
+                <FontAwesomeIcon icon={faFacebookF} className='icon' />
+              </div>
+              <h2>SMM</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, possimus.</p>
+            </div>
+          </div>
+
+          {/* IT */}
+          <div className="col-lg-3 col-md-6 col-sm-6">
+            <div className="box">
+              <div className="circle">
+                <FontAwesomeIcon icon={faDesktop} className='icon' />
+              </div>
+              <h2>IT, helpdesk</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, possimus.</p>
+            </div>
+          </div>
+
+
+
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Services 
